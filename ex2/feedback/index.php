7619f000f17c73254106a8783a1286e02588c9e7
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-23 18:21:39 +0300

    update code [ex2-51] добавление формы обратной связи на страницу

--- ex2/feedback/index.php
+++ ex2/feedback/index.php
@@ -1,5 +1,14 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 $APPLICATION->SetTitle("Форма обратной связи");
-?>
- <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+?><?$APPLICATION->IncludeComponent(
+	"bitrix:main.feedback",
+	"",
+	Array(
+		"EMAIL_TO" => "al.okorokova@mail.ru",
+		"EVENT_MESSAGE_ID" => array(),
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
+		"USE_CAPTCHA" => "Y"
+	)
+);?><? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
