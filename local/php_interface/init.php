851ad465ce0acf179b8a9f00506fe9930a2350b0
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-23 19:03:19 +0300

    update code [ex2-51] представление обработчика, как метода класса и добавление некоторого комментирования кода

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,13 +1,19 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 AddEventHandler('main', 'OnBeforeEventSend', "OnBeforeEventSendHandler");
-function OnBeforeEventSendHandler(&$arFields, &$arTemplate)
+
+/*
+Вызывается перед отправкой сообщения
+*/
+function OnBeforeEventSendHandler($arFields, $arTemplate)
 {
     global $USER;
 
     if ($USER->IsAuthorized()) {
+        /* если пользователь авторизован  */
         $arFields["AUTHOR"] = 'Пользователь авторизован: ' . $USER->GetID() . ' (' . $USER->GetLogin() . ') ' . $USER->GetFullName() . ', данные из формы: ' . $arFields["AUTHOR"];
     } else {
+        /* если пользователь не авторизован  */
         $arFields["AUTHOR"] = 'Пользователь не авторизован, данные из формы: ' . $arFields["AUTHOR"];
     }
 }
