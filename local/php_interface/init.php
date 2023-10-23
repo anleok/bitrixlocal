f17b179426d92ddbe870a5df7e087864b0eff9a3
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-23 19:22:43 +0300

    update code [ex2-51] добавление события в журнал событий

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,6 +1,7 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
 AddEventHandler('main', 'OnBeforeEventSend', array("userFeedbackForm", "OnBeforeEventSendHandler"));
+AddEventHandler('main', 'OnEventLogGetAuditTypes', array("userFeedbackForm", "OnEventLogGetAuditTypes"));
 
 class userFeedbackForm
 {
@@ -15,5 +16,19 @@ class userFeedbackForm
             /* если пользователь не авторизован  */
             $arFields["AUTHOR"] = 'Пользователь не авторизован, данные из формы: ' . $arFields["AUTHOR"];
         }
+
+        // добавляем событие в журнал событий
+        CEventLog::Add(array(
+            "SEVERITY" => "INFO",
+            "AUDIT_TYPE_ID" => "FEEDBACKFORM_AUTHOR_REWRITE_TYPE",
+            "MODULE_ID" => "main",
+            "ITEM_ID" => 123,
+            "DESCRIPTION" => "Замена данных в отсылаемом письме – " . $arFields["AUTHOR"],
+         ));
+    }
+
+    public static function OnEventLogGetAuditTypes()
+    {
+       return array('FEEDBACKFORM_AUTHOR_REWRITE_TYPE' => '[FEEDBACKFORM_AUTHOR_REWRITE_TYPE] Замена данных в отсылаемом письме');
     }
 }
