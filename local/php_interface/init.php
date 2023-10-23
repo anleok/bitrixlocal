b0e0054ab677ec3ff42c2cf052d9a7d27d292a8a
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-23 19:05:12 +0300

    update code [ex2-51] представление обработчика, как метода класса и добавление некоторого комментирования кода

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -1,19 +1,19 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 
-AddEventHandler('main', 'OnBeforeEventSend', "OnBeforeEventSendHandler");
+AddEventHandler('main', 'OnBeforeEventSend', array("userFeedbackForm", "OnBeforeEventSendHandler"));
 
-/*
-Вызывается перед отправкой сообщения
-*/
-function OnBeforeEventSendHandler($arFields, $arTemplate)
+class userFeedbackForm
 {
-    global $USER;
+    public static function OnBeforeEventSendHandler(&$arFields, &$arTemplate)
+    {
+        global $USER;
 
-    if ($USER->IsAuthorized()) {
-        /* если пользователь авторизован  */
-        $arFields["AUTHOR"] = 'Пользователь авторизован: ' . $USER->GetID() . ' (' . $USER->GetLogin() . ') ' . $USER->GetFullName() . ', данные из формы: ' . $arFields["AUTHOR"];
-    } else {
-        /* если пользователь не авторизован  */
-        $arFields["AUTHOR"] = 'Пользователь не авторизован, данные из формы: ' . $arFields["AUTHOR"];
+        if ($USER->IsAuthorized()) {
+            /* если пользователь авторизован  */
+            $arFields["AUTHOR"] = 'Пользователь авторизован: ' . $USER->GetID() . ' (' . $USER->GetLogin() . ') ' . $USER->GetFullName() . ', данные из формы: ' . $arFields["AUTHOR"];
+        } else {
+            /* если пользователь не авторизован  */
+            $arFields["AUTHOR"] = 'Пользователь не авторизован, данные из формы: ' . $arFields["AUTHOR"];
+        }
     }
 }
