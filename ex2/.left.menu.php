dc8237b82ba2ac77d2074d1682bf55aa1151848c
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-23 17:48:01 +0300

    create code [ex2-51] добавление пункта главного меню 'Экзамен2' и пункта левого меню 'Форма обратной связи' для проверки работы решения  в соответствии с заданием

---
+++ ex2/.left.menu.php
@@ -0,0 +1,12 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<?
+$aMenuLinks = array(
+	array(
+		"Форма обратной связи",
+		"feedback/",
+		array(),
+		array(),
+		""
+	),
+);
+?>
---
+++ ex2/feedback/index.php
@@ -0,0 +1,5 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetTitle("Форма обратной связи");
+?>
+ <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
