1421edd232d8f43117f538788c3f37b1f3d8e472
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-24 15:18:33 +0300

    update code [ex2-94] установка метаданных, если найден элемент в инфоблоке в соответствии с заданием

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -2,6 +2,7 @@
 
 AddEventHandler('main', 'OnBeforeEventSend', array("Ex2AllType", "OnBeforeEventSendHandler"));
 AddEventHandler('main', 'OnEventLogGetAuditTypes', array("Ex2AllType", "OnEventLogGetAuditTypes"));
+AddEventHandler('main', 'OnBeforeProlog', array("Ex2AllType", "OnBeforePrologHandler"));
 
 class Ex2AllType
 {
@@ -27,11 +28,28 @@ class Ex2AllType
             "MODULE_ID" => "main",
             "ITEM_ID" => 123,
             "DESCRIPTION" => "Замена данных в отсылаемом письме – " . $arFields["AUTHOR"],
-         ));
+        ));
     }
 
     public static function OnEventLogGetAuditTypes()
     {
-       return array('FEEDBACKFORM_AUTHOR_REWRITE_TYPE' => '[FEEDBACKFORM_AUTHOR_REWRITE_TYPE] Замена данных в отсылаемом письме');
+        return array('FEEDBACKFORM_AUTHOR_REWRITE_TYPE' => '[FEEDBACKFORM_AUTHOR_REWRITE_TYPE] Замена данных в отсылаемом письме');
     }
+
+    /* 
+    ex2-94
+    */
+    public static function OnBeforePrologHandler()
+    {
+        global $APPLICATION;    
+        $curPageDir = $APPLICATION->GetCurDir();
+
+        if (\Bitrix\Main\Loader::includeModule('iblock')) {
+            $rsMetaTags = CIBlockElement::GetList(array(), array("IBLOCK_TYPE" => "products", "IBLOCK_ID" => 5, "ACTIVE" => "Y", "NAME" => $curPageDir), false, false, array("PROPERTY_TITLE", "PROPERTY_DESCRIPTION"));
+            if ($arMetaTags = $rsMetaTags->Fetch()) {
+                $APPLICATION->SetPageProperty("title", $arMetaTags["PROPERTY_TITLE_VALUE"]);
+                $APPLICATION->SetPageProperty("description", $arMetaTags["PROPERTY_DESCRIPTION_VALUE"]);
+            }
+        }
+    } 
 }
