720af80bde145fc991a30130514f1809c3ac155d
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-26 15:58:27 +0300

    update code [simplecomp] создание параметров компонента

--- local/components/simplecomp/.parameters.php
+++ local/components/simplecomp/.parameters.php
@@ -3,8 +3,31 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 $arComponentParameters = array(
 	"PARAMETERS" => array(
 		"PRODUCTS_IBLOCK_ID" => array(
-			"NAME" => GetMessage("SIMPLECOMP_EXAM2_CAT_IBLOCK_ID"),
+			"NAME" => GetMessage("SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"),
+			"PARENT" => "BASE",
 			"TYPE" => "STRING",
 		),
+		"CLASSIFIER_IBLOCK_ID" => array(
+			"NAME" => GetMessage("SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"),
+			"PARENT" => "BASE",
+			"TYPE" => "STRING",
+		),
+		"DETAIL_PAGE" => array(
+			"NAME" => GetMessage("SIMPLECOMP_EXAM2_DETAIL_PAGE"),
+			"PARENT" => "BASE",
+			"TYPE" => "STRING",
+		),
+		"PROPERTY_CODE" => array(
+			"NAME" => GetMessage("SIMPLECOMP_EXAM2_PROPERTY_CODE"),
+			"PARENT" => "BASE",
+			"TYPE" => "STRING",
+		),
+		"CACHE_TIME"  =>  ["DEFAULT"=>36000000],
+		"CACHE_GROUPS" => [
+			"PARENT" => "CACHE_SETTINGS",
+			"NAME" => GetMessage("SIMPLECOMP_EXAM2_CACHE_GROUPS"),
+			"TYPE" => "CHECKBOX",
+			"DEFAULT" => "Y",
+		],
 	),
 );
--- local/components/simplecomp/lang/ru/.parameters.php
+++ local/components/simplecomp/lang/ru/.parameters.php
@@ -1,3 +1,7 @@
 <?
-$MESS["SIMPLECOMP_EXAM2_CAT_IBLOCK_ID"] = "ID инфоблока с каталогом товаров";
+$MESS["SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"] = "ID инфоблока с каталогом товаров";
+$MESS["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] = "ID инфоблока с классификатором";
+$MESS["SIMPLECOMP_EXAM2_DETAIL_PAGE"] = "Шаблон ссылки на детальный просмотр товара";
+$MESS["SIMPLECOMP_EXAM2_PROPERTY_CODE"] = "Код свойства товара, в котором хранится привязка товара к классификатору";
+$MESS["SIMPLECOMP_EXAM2_CACHE_GROUPS"] = "Учитывать права доступа";
 ?>
