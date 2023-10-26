cea82bb0393ede16b387fe8fd0cd1725954a46a0
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-26 17:13:36 +0300

    update code [simplecomp component] исправление параметров и получение инфы о классификаторах

--- local/components/simplecomp/component.php
+++ local/components/simplecomp/component.php
@@ -10,12 +10,12 @@ if(!Loader::includeModule("iblock"))
 	return;
 }
 
-if (empty($arParams["SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"])) {
-    $arParams["SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"] = 0;
+if (empty($arParams["PRODUCTS_IBLOCK_ID"])) {
+    $arParams["PRODUCTS_IBLOCK_ID"] = 0;
 }
 
-if (empty($arParams["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"])) {
-	$arParams["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] = 0;
+if (empty($arParams["CLASSIFIER_IBLOCK_ID"])) {
+	$arParams["SCLASSIFIER_IBLOCK_ID"] = 0;
 }
 
 if (!isset($arParams["CACHE_TIME"]))
@@ -23,13 +23,35 @@ if (!isset($arParams["CACHE_TIME"]))
 	$arParams["CACHE_TIME"] = 36000000;
 }
 
-$arParams["SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"] = trim($arParams["SIMPLECOMP_EXAM2_PRODUCTS_IBLOCK_ID"] ?? '');
-$arParams["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] = trim($arParams["SIMPLECOMP_EXAM2_CLASSIFIER_IBLOCK_ID"] ?? '');
-$arParams["SIMPLECOMP_EXAM2_PROPERTY_CODE"] = trim($arParams["SIMPLECOMP_EXAM2_PROPERTY_CODE"] ?? '');
+$arParams["PRODUCTS_IBLOCK_ID"] = trim($arParams["PRODUCTS_IBLOCK_ID"] ?? '');
+$arParams["CLASSIFIER_IBLOCK_ID"] = trim($arParams["CLASSIFIER_IBLOCK_ID"] ?? '');
+$arParams["PROPERTY_CODE"] = trim($arParams["PROPERTY_CODE"] ?? '');
 
+echo '<pre>' . print_r( $arParams, true ) . '</pre>';
 global $USER;
 if($this->startResultCache(false, array(($arParams["CACHE_GROUPS"]==="N"? false: $USER->GetGroups())))){
+	// классификаторы
+	$arResult["CLASSIFIER_COUNT"] = 0;
+	$arSelectElements = array(
+        "ID",
+        "NAME"
+    );
+    $arFilterElements = array(
+		"IBLOCK_ID" => $arParams["CLASSIFIER_IBLOCK_ID"],
+        "ACTIVE" => "Y",
+        "CHECK_PERMISSIONS" => $arParams["CACHE_GROUPS"]
+    );
 
+    $rsElements = CIBlockElement::GetList(array(), $arFilterElements, false, false, $arSelectElements);
+
+    while($arElement = $rsElements->GetNext()) {
+        $arClassifier[$arElement["ID"]] = $arElement;
+        $arClassifierID[] = $arElement["ID"];
+    }
+
+	if (is_countable($arClassifierID)) {
+		$arResult["CLASSIFIER_COUNT"] = count($arClassifierID);
+	}
 }
 
 if(intval($arParams["PRODUCTS_IBLOCK_ID"]) > 0)
