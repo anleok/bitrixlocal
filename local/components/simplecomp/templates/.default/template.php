0496c512d9bc8d895c98cfb74026a963ddd5f1e0
Anastasia Okorokova <al.okorokova@mail.ru>
2023-10-26 18:08:17 +0300

    update code [simplecomp template] отображение списка в шаблоне

--- local/components/simplecomp/templates/.default/template.php
+++ local/components/simplecomp/templates/.default/template.php
@@ -1,7 +1,18 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
-<p><b><?=GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE")?></b></p>
-<pre>
-<?
-print_r($arResult);
-?>
-</pre>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<p><b><?= GetMessage("SIMPLECOMP_EXAM2_CAT_TITLE") ?></b></p>
+<? if (is_countable($arResult["CLASSIFIER"]) && count($arResult["CLASSIFIER"]) > 0) { ?>
+    <ul>
+        <? foreach ($arResult["CLASSIFIER"] as $arClassifier) { ?>
+            <li>
+                <b><?= $arClassifier["NAME"]; ?></b>
+                <? if (is_countable($arClassifier["ELEMENTS"]) && count($arClassifier["ELEMENTS"]) > 0) { ?>
+                    <ul>
+                        <? foreach ($arClassifier["ELEMENTS"] as $arItem) { ?>
+                            <li><?= $arItem["NAME"]; ?> - <?= $arItem["PROPERTIES"]["PRICE"]["VALUE"]; ?> - <?= $arItem["PROPERTIES"]["MATERIAL"]["VALUE"]; ?> - <?= $arItem["PROPERTIES"]["ARTNUMBER"]["VALUE"]; ?> - <a href="<?= $arItem["DETAIL_PAGE_URL"]; ?>">подробнее</a></li>
+                        <? } ?>
+                    </ul>
+                <? } ?>
+            </li>
+        <? } ?>
+    </ul>
+<? } ?>
